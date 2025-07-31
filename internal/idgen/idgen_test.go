7be9e215af96e89f858b3ec04e8092e95751700e
Nick Perkins <nick@nickperkins.au>
2025-07-31 11:15:55 +1000

    refactor: Remove unnecessary blank lines in test files

--- internal/idgen/idgen_test.go
+++ internal/idgen/idgen_test.go
@@ -1,10 +1,9 @@
-
 package idgen
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestGenerateTaskID_UniqueAndValid(t *testing.T) {
